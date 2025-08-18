1754d00099f7b0b04bd0bcc547c86451457b6e1a
Sazzad <sazzad.mzd@gmail.com>
2025-08-18 11:17:55 +0600

    Class 01

---
+++ Assignment_Task2_Task3/Assignment_Task2_Task3.cs
@@ -0,0 +1,82 @@
+﻿namespace Assignment_Task2_Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 02:
+
+            //Declare an integer array of size 5.Take input from the Console Window using loop.
+
+            int[] numbers = new int[5];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("Enter number " + (i + 1) + ": ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("numbers = ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] +", ");
+            }
+            Console.WriteLine(" ");
+
+            //Task 03
+
+            //Declare an integer array of size 3.Insert 3 random numbers.
+
+            int[] numArray = new int[3];
+            Random random = new Random();
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                numArray[i] = random.Next(1, 100);
+            }
+
+            Console.Write("numArray = ");
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                Console.Write(numArray[i] + ", ");
+            }
+            Console.WriteLine(" ");
+
+            //Find the largest number using conditional statement (if-else)
+
+            int largest = numArray[0];
+            if (largest < numArray[1])
+            {
+                largest = numArray[1];
+                if (largest < numArray[2])
+                {
+                    largest = numArray[2];
+                }
+            }
+
+
+            Console.WriteLine("Largest Number in Array: " + largest);
+
+            //Find the smallest number using ‘Switch’ statement
+
+            int smallest = numArray[0];
+            switch (smallest > numArray[1])
+            {   
+                case true:
+                    smallest = numArray[1];
+                    break;
+
+                default:
+                    break;
+            }
+            switch (smallest > numArray[2])
+            {
+                case true:
+                    smallest = numArray[2];
+                    break;
+
+                default:
+                    break;
+            }
+
+            Console.WriteLine("Smallest Number in Array: " + smallest);
+        }
+    }
+}
---
+++ class01/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace class01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 42;
+            int age = 30;
+
+            double height = 5.9;
+
+            int x = (int)5.9;
+
+            string fullName = "John Doe";
+
+            string number1 = "10";
+            string number2 = "20";
+
+            //Console.WriteLine(number1 + number2);
+
+            bool isAlive = true;
+
+            string[] names = new string[3];
+            names[0] = "Alice";
+            names[1] = "Bob";
+            names[2] = "Charlie";
+
+            //switch case example
+            //take int input in array using loop
+
+            //Task 01:
+            //-------
+
+
+            //Declare an integer array of size 5.Take input from the Console Window using loop.
+
+            int[] numbers = new int[5];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                //Console.Write("Enter number " + (i + 1) + ": ");
+                //numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Declare an integer array of size 3.Insert 3 random numbers.
+
+            int[] numbersArray = {1, 5, 9};
+
+            //Find the largest number using conditional statement (if-else)
+
+            int largest = numbersArray[0];
+            if(largest < numbersArray[1])
+            {
+                largest = numbersArray[1];
+                if(largest < numbersArray[2])
+                {
+                    largest = numbersArray[2];
+                }
+            }
+            
+
+            Console.WriteLine("Largest Number in numbersArray: " + largest);
+
+            //Find the smallest number using ‘Switch’ statement
+
+            int smallest = numbersArray[0];
+            switch (smallest > numbersArray[1])
+            {
+                case true:
+                    smallest = numbersArray[1];
+                    break;
+                
+                default:
+                    break;
+            }
+            switch (smallest > numbersArray[2])
+            {
+                case true:
+                    smallest = numbersArray[2];
+                    break;
+                
+                default:
+                    break;
+            }
+
+            Console.WriteLine("Smallest Number in numbersArray: " + smallest);
+
+
+
+
+        }
+    }
+}
